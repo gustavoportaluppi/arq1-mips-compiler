280ac896a6e56998ef490da39453d532537dd78c
GustavoPortaluppi <portaluppigustavo@gmail.com>
2019-06-06 11:06:49 -0300

    feat(general): regs + tests

--- src/app/test-two/test-two.component.ts
+++ src/app/test-two/test-two.component.ts
@@ -18,6 +18,10 @@ export class TestTwoComponent implements OnInit {
 
   operands = [];
 
+  data = '.data';
+  text = '.text';
+  exec = '';
+
   constructor() {
   }
 
@@ -30,7 +34,10 @@ export class TestTwoComponent implements OnInit {
 
     exp.forEach((el, i) => {
       if (!isNaN(el)) {
-        this.operands.push(el);
+        const reg = this.registers.pop();
+        // console.log(`${reg}: ${el}`);
+        this.initData(reg, el);
+        this.operands.push(reg);
       } else {
         if (el === '+') {
           this.operands.push(this.process('add'));
@@ -46,16 +53,38 @@ export class TestTwoComponent implements OnInit {
         }
       }
     });
+
+    this.text += `\n${this.exec}`;
+    this.text += `\n\n  sw  $5, 0(${this.operands[0]})`;
+
+
+    console.log(this.data);
+    console.log(this.text);
+  }
+
+  initData(reg: string, value: string) {
+    const varName = `var_${reg.replace('$', '')}`;
+    this.data += `\n  ${varName}: .word  0x${value}`;
+    this.initText(reg, varName);
+  }
+
+  initText(reg: string, varName: string) {
+    this.text += `\n  la	${reg}, ${varName}`;
+    this.text += `\n  lw	${reg}, 0(${reg})`;
   }
 
   process(op: string) {
-    this.printOp(op, this.operands.pop(), this.operands.pop());
-    return op;
+    const ref1 = this.operands.pop();
+    const ref2 = this.operands.pop();
+    this.printOp(op, ref2, ref2, ref1);
+    return ref2;
   }
 
-  printOp(op: string, op1: string, op2: string) {
+  printOp(op: string, op1: string, op2: string, op3: string) {
     let s = `${op}`;
-    s += ` ${op2} ${op1}`;
+    s += `  ${op1} ${op2} ${op3}`;
+
+    this.exec += `\n  ${s}`;
     console.log(`>>>>>> ${s}`);
   }
 
